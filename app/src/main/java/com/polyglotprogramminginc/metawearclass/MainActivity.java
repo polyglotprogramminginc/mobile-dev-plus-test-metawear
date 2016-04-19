24f97a34259b5594bd71661947df23394998b55b
Lance Gleason <lgleasain@yahoo.com>
2016-04-19 11:47:17 -0700

    initial metawear skeleton app

---
+++ app/src/main/java/com/polyglotprogramminginc/metawearclass/MainActivity.java
@@ -0,0 +1,63 @@
+package com.polyglotprogramminginc.metawearclass;
+
+import android.content.ComponentName;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mbientlab.metawear.MetaWearBleService;
+
+public class MainActivity extends AppCompatActivity implements ServiceConnection {
+    private MetaWearBleService.LocalBinder mwBinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder service) {
+        ///< Get a reference to the MetaWear service from the binder
+        mwBinder = (MetaWearBleService.LocalBinder) service;
+        Log.i("Main Activity", "Service Connected");
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        Log.i("Main Activity", "Service Disconnected");
+    }
+}
